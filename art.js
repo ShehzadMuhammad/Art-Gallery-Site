08eb06b0e6608b41fed823bf4d6be9ab6d9989ef
ShehzadMuhammad <shehzad.muhammad@ryerson.ca>
2018-03-11 12:46:58 -0400

    Create art.js

---
+++ art.js
@@ -0,0 +1,230 @@
+	function artwork1() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src = "Resources/Monalisa.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArt1()'> Mona Lisa</span> </br> Date of Production: 1503–06 <br>Price: $800 M";
+	}
+
+	function showArt1() {
+		document.getElementById("artImage").src = "Resources/Monalisa.jpg";
+		document.getElementById("artDescription").innerHTML = "Date of Production: 1503–06, perhaps continuing until c. 1517<br>Type of the Painting Color: Oil<br>Dimensions: 65 cm x 90 cm<br>Location: Musée du Louvre, Paris<br>Artist: Leonardo da Vinci<br>Price: $800 M<br>Genre: Portrait";
+	}
+
+	function artwork2() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src = "Resources/night.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArt2()'>The Night Watch</span> </br> Description: 1503–06 <br>Price: $23,000";
+	}
+
+	function showArt2() {
+		document.getElementById("artImage").src = "Resources/night.jpg";
+		document.getElementById("artDescription").innerHTML ="Date of Production: 1642<br>Type of the Painting Color: Oil<br>Dimensions: 363 cm × 437 cm<br>Location: Amsterdam Museum, Paris<br>Artist: Rembrandt van Rijn<br>Price: $23,000<br>Genre: Baroque";
+	}
+
+	function artwork3() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src = "Resources/thirdclass.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArt3()'>Third Class Carriage</span> </br> Date of Production: 1503–06 <br>Price: $40,000";
+	}
+
+	function showArt3() {
+		document.getElementById("artImage").src = "Resources/thirdclass.jpg";
+		document.getElementById("artDescription").innerHTML = "Date of Production: 1862–1864<br>Type of the Painting Color: Oil<br>Dimensions: 65.4 cm × 90.2 cm<br>Location: Metropolitan Museum of Art<br>Artist: Honoré Daumier<br>Price: $40,000<br>Genre: Realism";
+	}
+
+	function artwork4() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src = "Resources/starry.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArt4()'> The Starry Night</span> </br>General Description: June 1889 <br> Price: $150 M";
+	}
+
+	function showArt4() {
+		document.getElementById("artImage").src = "Resources/starry.jpg";
+		document.getElementById("artDescription").innerHTML = "Date of Production: June 1889<br>Type of the Painting Color: Oil<br>Dimensions: 74 cm x 92 cm<br>Location: Museum of Modern Art<br>Artist: Vincent van Gogh<br>Price: $150 M<br>Genre: Landscape";
+	}
+
+	function artwork5() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src = "Resources/guer.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArt5()'> Guernica</span> </br> Date of Production: 1503–06 <br>Price: $179 M";
+	}
+
+	function showArt5() {
+		document.getElementById("artImage").src = "Resources/guer.jpg";
+		document.getElementById("artDescription").innerHTML =  "Date of Production: April 26, 1937–June 1937<br>Type of the Painting Color: Oil<br>Dimensions: 3.49 m x 7.77 m<br>Location: Museo Nacional Centro de Arte Reina Sofía<br>Artist: Pablo Picasso<br>Price: $179 M<br>Genre: Abstract";
+	}
+
+
+		function artist1() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src = "Resources/raph.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArtist1()'> Raphael Sanzio da Urbino</span> </br> Date of Production: 1503–06 <br>Price: $800 M";
+	}
+
+	function showArtist1() {
+		document.getElementById("artImage").src = "Resources/raph.jpg";
+		document.getElementById("artDescription").innerHTML = "Date of birth: 1483<br> deathof the artist: 1520<br> Place of his/her living: Urbino, Italy<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): Baroque, Renaissance<br> Famous of his/her piece of works: \"The School of Athens\", \"The Sistine Madonna\", \"The Marriage of the Virgin\"";
+	}
+
+	function artist2() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src = "Resources/vinci.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArtist2()'> Leonardo di ser Piero da Vinci</span> </br> Date of Production: 1503–06 <br>Price: $800 M";
+	}
+
+	function showArtist2() {
+		document.getElementById("artImage").src = "Resources/vinci.jpg";
+		document.getElementById("artDescription").innerHTML = "Date of birth: 1452<br> Death of the artist: 1519<br> Place of his/her living: Anchiano, Italy<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): Renaissance<br> Famous of his/her piece of works: \"Mona Lisa\", \"The Last Supper\", \"St. John the Baptist\"";
+	}
+
+	function artist3() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src =  "Resources/rembrandt.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArtist3()'> Rembrandt Harmenszoon van Rjin</span> </br> Date of Production: 1503–06 <br>Price: $800 M";
+	}
+
+	function showArtist3() {
+		document.getElementById("artImage").src = "Resources/rembrandt.jpg";
+		document.getElementById("artDescription").innerHTML = "Date of birth: 1606<br> Death of the artist: 1669<br> Place of his/her living: Leiden, Netherlands<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): Baroque, Realism, Post-Impressionism<br> Famous of his/her piece of works: \"The Night Watch\", \"The Return of the Prodigal\", \"The Jewish Bride\"";
+	}
+
+	function artist4() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src =  "Resources/picasso.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArtist4()'> Pablo Picasso</span> </br> Date of Production: 1503–06 <br>Price: $800 M";
+	}
+
+	function showArtist4() {
+		document.getElementById("artImage").src = "Resources/picasso.jpg";
+		document.getElementById("artDescription").innerHTML = "Date of birth: 1881<br> Death of the artist: 1973<br> Place of his/her living: Malaga, Spain<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): Cubism, Surrealism, Expressionism, Post=Post-Impressionism<br> Famous of his/her piece of works: \"Guernica\", \"Les Demoiselles d'Avignon\", \"The Old Guitarist\"";
+	}
+
+	function artist5() {
+		document.getElementById("ArtSection").style.visibility = "visible";
+		document.getElementById("artImage").src =  "Resources/angelo.jpg";
+		document.getElementById("artDescription").innerHTML = "Name: <span onClick='showArtist5()'> Michelangelo</span> </br> Date of Production: 1503–06 <br>Price: $800 M";
+	}
+
+	function showArtist5() {
+		document.getElementById("artImage").src = "Resources/angelo.jpg";
+		document.getElementById("artDescription").innerHTML = "Date of birth: 1475<br> Death of the artist: 1564<br> Place of his/her living: Caprese Michelangelo, Italy<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): High Renaissance, Italian Renaissance, Renaissance<br> Famous of his/her piece of works: \"Sistine Chapel ceiling\", \"The Last Judgment\", \"David\"";
+	}
+
+
+
+	
+	function smallData(name) {
+				document.getElementById("ArtSection").style.visibility = "visible";
+				var artsrc = "";
+				var description = "";
+				/*Artist Switch*/
+				
+				/*Museum Switch*/
+				switch (name){
+					case "The Louvre":
+						artsrc = "Resources/Louvre.jpg";
+						description = "Description";
+						break;
+					case "Metropolitan Museum of Art":
+						artsrc = "Resources/met-museum.jpg";
+						description = "Description";
+						break;
+					case "Hermitage Museum":
+						artsrc = "Resources/hermitage.jpg";
+						description = "Description";
+						break;
+					case "British Museum":
+						artsrc = "Resources/british.jpg";
+						description = "Description";
+						break;
+					case "Art Institute of Chicago":
+						artsrc = "Resources/chicago.jpg";
+						description = "Description";
+						break;
+				}
+				document.getElementById("artImage").src = artsrc;
+				document.getElementById("artDescription").innerHTML = description;
+			}
+
+	function showData(name) {
+				document.getElementById("ArtSection").style.visibility = "visible";
+				var artsrc = "";
+				var description = "";
+				/*Art Switch*/
+				switch (name){
+					case "Mona Lisa":
+						artsrc = "Resources/Monalisa.jpg";
+						description = "Name: Mona Lisa <br> Date of Production: 1503–06, perhaps continuing until c. 1517<br>Type of the Painting Color: Oil<br>Dimensions: 65 cm x 90 cm<br>Location: Musée du Louvre, Paris<br>Artist: Leonardo da Vinci<br>Price: $800 M<br>Genre: Portrait"
+						break;
+
+					case "The Night Watch":
+						artsrc = "Resources/night.jpg";
+						description = "Date of Production: 1642<br>Type of the Painting Color: Oil<br>Dimensions: 363 cm × 437 cm<br>Location: Amsterdam Museum, Paris<br>Artist: Rembrandt van Rijn<br>Price: $23,000<br>Genre: Baroque"
+						break;
+
+					case "Third Class Carriage":
+						artsrc = "Resources/thirdclass.jpg";
+						description = "Date of Production: 1862–1864<br>Type of the Painting Color: Oil<br>Dimensions: 65.4 cm × 90.2 cm<br>Location: Metropolitan Museum of Art<br>Artist: Honoré Daumier<br>Price: $40,000<br>Genre: Realism"
+						break;
+
+					case "The Starry Night":
+						artsrc = "Resources/starry.jpg";
+						description = "Date of Production: June 1889<br>Type of the Painting Color: Oil<br>Dimensions: 74 cm x 92 cm<br>Location: Museum of Modern Art<br>Artist: Vincent van Gogh<br>Price: $150 M<br>Genre: Landscape"
+						break;
+
+					case "Guernica":
+						artsrc = "Resources/guer.jpg";
+						description = "Date of Production: April 26, 1937–June 1937<br>Type of the Painting Color: Oil<br>Dimensions: 3.49 m x 7.77 m<br>Location: Museo Nacional Centro de Arte Reina Sofía<br>Artist: Pablo Picasso<br>Price: $179 M<br>Genre: Abstract"
+						break;
+				}
+				/*Artist Switch*/
+				switch (name){
+					case "Raphael Sanzio da Urbino":
+						artsrc = "Resources/raph.jpg";
+						description = "Artist: Raphael Sanzio da Urbino<br> Date of birth: 1483<br> deathof the artist: 1520<br> Place of his/her living: Urbino, Italy<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): Baroque, Renaissance<br> Famous of his/her piece of works: \"The School of Athens\", \"The Sistine Madonna\", \"The Marriage of the Virgin\"";
+						break;
+					case "Leonardo di ser Piero da Vinci":
+						artsrc = "Resources/vinci.jpg";
+						description = "Artist: Leonardo di ser Piero da Vinci<br> Date of birth: 1452<br> Death of the artist: 1519<br> Place of his/her living: Anchiano, Italy<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): Renaissance<br> Famous of his/her piece of works: \"Mona Lisa\", \"The Last Supper\", \"St. John the Baptist\"";
+						break;
+					case "Rembrandt Harmenszoon van Rjin":
+						artsrc = "Resources/rembrandt.jpg";
+						description = "Artist: Rembrandt Harmenszoon van Rjin<br> Date of birth: 1606<br> Death of the artist: 1669<br> Place of his/her living: Leiden, Netherlands<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): Baroque, Realism, Post-Impressionism<br> Famous of his/her piece of works: \"The Night Watch\", \"The Return of the Prodigal\", \"The Jewish Bride\"";
+						break;
+
+					case "Pablo Picasso":
+						artsrc = "Resources/picasso.jpg";
+						description = "Artist: Pablo Picasso<br> Date of birth: 1881<br> Death of the artist: 1973<br> Place of his/her living: Malaga, Spain<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): Cubism, Surrealism, Expressionism, Post=Post-Impressionism<br> Famous of his/her piece of works: \"Guernica\", \"Les Demoiselles d'Avignon\", \"The Old Guitarist\"";
+						break;
+
+					case "Michelangelo":
+						artsrc = "Resources/angelo.jpg";
+						description = "Artist: Michelangelo di Lodovico Buonarroti Simoni <br> Date of birth: 1475<br> Death of the artist: 1564<br> Place of his/her living: Caprese Michelangelo, Italy<br> Genres of his/her paintings(e.g. Gothic, Renaissance, Baroque, Pre-Modern, and Modern): High Renaissance, Italian Renaissance, Renaissance<br> Famous of his/her piece of works: \"Sistine Chapel ceiling\", \"The Last Judgment\", \"David\"";
+						break;
+
+				}
+				/*Museum Switch*/
+				switch (name){
+					case "The Louvre":
+						artsrc = "Resources/Louvre.jpg";
+						description = "Description";
+						break;
+					case "Metropolitan Museum of Art":
+						artsrc = "Resources/met-museum.jpg";
+						description = "Description";
+						break;
+					case "Hermitage Museum":
+						artsrc = "Resources/hermitage.jpg";
+						description = "Description";
+						break;
+					case "British Museum":
+						artsrc = "Resources/british.jpg";
+						description = "Description";
+						break;
+					case "Art Institute of Chicago":
+						artsrc = "Resources/chicago.jpg";
+						description = "Description";
+						break;
+				}
+				document.getElementById("artImage").src = artsrc;
+				document.getElementById("artDescription").innerHTML = description;
+			}
